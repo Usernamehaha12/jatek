b3c6b8ba0bb529cb0841be85c5e6d8098ba06f4b
ButtyMate00 <118981147+ButtyMate00@users.noreply.github.com>
2023-03-02 13:22:04 +0100

    jatekos
    damage.hp,skill,luck stb szamolas

---
+++ json.py
@@ -0,0 +1,11 @@
+#with open("nev.json", "r", encoding="utf-8") as f:
+#    data = json.load(f)
+
+#    print(data["bevezető"])
+#k = 0
+
+#print(data["kartyak"][k]["szoveg"])
+#szoveg = data["kartyak"][k]["szoveg"]
+#akcio = data["kartyak"][k]["akcio"]
+#if akcio["tipus"] == "ugras":
+#    print(szoveg)
--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import json
 import random
 
-with open("kaland.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-
-
-import random
-
 class Character:
-    def __init__(self, name, skill, health, damage):
+    def __init__(self,name, skill = 6, health = 12,damage = 0 , luck = 6):
         self.name = name
-        self.skill = skill
-        self.health = health
+        self.skill = int(skill)
+        self.health = int(health)
         self.damage = damage
+        self.luck = luck
 
     def generate_skill(self):
-        self.skill = random.randint(1, 100)
+        self.skill += random.randint(1, 6)
+
+    def generate_health(self):
+        self.health += random.randint(1, 6)
+
+    def generate_luck(self):
+        self.luck += random.randint(1, 6)
+
+    def generate_damage(self):
+        self.damage = self.luck + random.randint(1, 6)
 
 
-a = Character("jatekos",1 ,1,1 )
-a.generate_skill
-a.skill
+a = Character("jatekos")
 
+a.generate_skill()
+a.generate_health()
+a.generate_luck()
+a.generate_damage()
+print(a.skill, a.health, a.luck, a.damage)

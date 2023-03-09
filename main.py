e3ab42c5017cacb5e0258a103404d0061618a311
ButtyMate00 <118981147+ButtyMate00@users.noreply.github.com>
2023-03-09 11:12:19 +0100

    .
    .

--- main.py
+++ main.py
@@ -172,135 +172,135 @@ csatamod = 0
 input("új írj be valamit van enter : ")
 Player = Player(dobasok() + 12, dobas() + 6, dobas() + 6, 20)
 with open("Kaland.json", "r", encoding="utf-8") as jsn:
-    advDict = json.load(jsn)
+    Kaland = json.load(jsn)
 while not Nyert:
     LepettEMar = False
     Player.lokostr()
-    print(advDict['kaland'][Player.lokacio]['szoveg'] + "\n")                                                  
-    if "eleterovesztes" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.jatekosSebzes(advDict['kaland'][Player.lokacio]['ertek'])
-    if "hplossrng" in advDict['kaland'][Player.lokacio]['akcio']:
+    print(Kaland['kaland'][Player.lokacio]['szoveg'] + "\n")                                                  
+    if "eleterovesztes" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.jatekosSebzes(Kaland['kaland'][Player.lokacio]['ertek'])
+    if "hplossrng" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.jatekosSebzes(dobas())
-    if "szerencsevesztes" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.nemluck(advDict['kaland'][Player.lokacio]['ertek'])
-    if "Eleteronyeres" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.jatekosHeal(advDict['kaland'][Player.lokacio]['ertek'])
-    if "szerencsenyeres" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.addluck(advDict['kaland'][Player.lokacio]['ertek'])
-    if "luckblessing" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "szerencsevesztes" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.nemluck(Kaland['kaland'][Player.lokacio]['ertek'])
+    if "Eleteronyeres" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.jatekosHeal(Kaland['kaland'][Player.lokacio]['ertek'])
+    if "szerencsenyeres" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.addluck(Kaland['kaland'][Player.lokacio]['ertek'])
+    if "luckblessing" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.PlayerBls()
-    if "combatblessing" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "combatblessing" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.PlayerCBls()
-    if "startluck" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.startluck(advDict['kaland'][Player.lokacio]['ertek'])
-    if "szerencse+hpminusz" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.nemluck(advDict['kaland'][Player.lokacio]['ertek'][0])
-        Player.jatekosSebzes(advDict['kaland'][Player.lokacio]['ertek'][1])
-    if "elix" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "startluck" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.startluck(Kaland['kaland'][Player.lokacio]['ertek'])
+    if "szerencse+hpminusz" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.nemluck(Kaland['kaland'][Player.lokacio]['ertek'][0])
+        Player.jatekosSebzes(Kaland['kaland'][Player.lokacio]['ertek'][1])
+    if "elix" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.SzerencseElix()
-    if "starttort" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "starttort" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.starttort()
-    if "pluszlebeges" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "pluszlebeges" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.additem("Lebegés Köpenye")
-    if "pluszgyuru" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "pluszgyuru" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.additem("Ügyesség Gyürüje")
-    if "pluszarany" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "pluszarany" in Kaland['kaland'][Player.lokacio]['akcio']:
         Player.additem("Aranykulcs")
-    if "+penz" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.addcash(int(advDict['kaland'][Player.lokacio]['mennyiseg'][0]))
-    if "+kristaly" in advDict['kaland'][Player.lokacio]['akcio']:
-        Player.addcrys(advDict['kaland'][Player.lokacio]['targy'])
-    if "csatamod" in advDict['kaland'][Player.lokacio]['akcio']:
-        csatamod = int(advDict['kaland'][Player.lokacio]['ertek'][0])
-    if "onharc" in advDict['kaland'][Player.lokacio]['akcio']:
-        if advDict['kaland'][Player.lokacio]['ellenfelek'] == 1:
+    if "+penz" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.addcash(int(Kaland['kaland'][Player.lokacio]['mennyiseg'][0]))
+    if "+kristaly" in Kaland['kaland'][Player.lokacio]['akcio']:
+        Player.addcrys(Kaland['kaland'][Player.lokacio]['targy'])
+    if "csatamod" in Kaland['kaland'][Player.lokacio]['akcio']:
+        csatamod = int(Kaland['kaland'][Player.lokacio]['ertek'][0])
+    if "onharc" in Kaland['kaland'][Player.lokacio]['akcio']:
+        if Kaland['kaland'][Player.lokacio]['ellenfelek'] == 1:
             harc(csatamod, "te", Player.HP, Player.Skill)
             csatamod = 0
-    if "harc" in advDict['kaland'][Player.lokacio]['akcio']:
-        if advDict['kaland'][Player.lokacio]['ellenfelek'] == 1:
-            harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel']['nev'],
-                 advDict['kaland'][Player.lokacio]['ellenfel']['HP'],
-                 advDict['kaland'][Player.lokacio]['ellenfel']['ugyesseg'])
+    if "harc" in Kaland['kaland'][Player.lokacio]['akcio']:
+        if Kaland['kaland'][Player.lokacio]['ellenfelek'] == 1:
+            harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel']['nev'],
+                 Kaland['kaland'][Player.lokacio]['ellenfel']['HP'],
+                 Kaland['kaland'][Player.lokacio]['ellenfel']['ugyesseg'])
             csatamod = 0
-        elif advDict['kaland'][Player.lokacio]['ellenfelek'] == 2:
-            if harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel']['nev'],
-                    advDict['kaland'][Player.lokacio]['ellenfel']['HP'],
-                    advDict['kaland'][Player.lokacio]['ellenfel']['ugyesseg']):
+        elif Kaland['kaland'][Player.lokacio]['ellenfelek'] == 2:
+            if harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel']['nev'],
+                    Kaland['kaland'][Player.lokacio]['ellenfel']['HP'],
+                    Kaland['kaland'][Player.lokacio]['ellenfel']['ugyesseg']):
                 csatamod = 0
-                harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel2']['nev'],
-                     advDict['kaland'][Player.lokacio]['ellenfel2']['HP'],
-                     advDict['kaland'][Player.lokacio]['ellenfel2']['ugyesseg'])
-        elif advDict['kaland'][Player.lokacio]['ellenfelek'] == 3:
-            if harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel']['nev'],
-                    advDict['kaland'][Player.lokacio]['ellenfel']['HP'],
-                    advDict['kaland'][Player.lokacio]['ellenfel']['ugyesseg']):
+                harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel2']['nev'],
+                     Kaland['kaland'][Player.lokacio]['ellenfel2']['HP'],
+                     Kaland['kaland'][Player.lokacio]['ellenfel2']['ugyesseg'])
+        elif Kaland['kaland'][Player.lokacio]['ellenfelek'] == 3:
+            if harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel']['nev'],
+                    Kaland['kaland'][Player.lokacio]['ellenfel']['HP'],
+                    Kaland['kaland'][Player.lokacio]['ellenfel']['ugyesseg']):
                 csatamod = 0
-                if harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel2']['nev'],
-                        advDict['kaland'][Player.lokacio]['ellenfel2']['HP'],
-                        advDict['kaland'][Player.lokacio]['ellenfel2']['ugyesseg']):
-                    harc(csatamod, advDict['kaland'][Player.lokacio]['ellenfel3']['nev'],
-                         advDict['kaland'][Player.lokacio]['ellenfel3']['HP'],
-                         advDict['kaland'][Player.lokacio]['ellenfel3']['ugyesseg'])
+                if harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel2']['nev'],
+                        Kaland['kaland'][Player.lokacio]['ellenfel2']['HP'],
+                        Kaland['kaland'][Player.lokacio]['ellenfel2']['ugyesseg']):
+                    harc(csatamod, Kaland['kaland'][Player.lokacio]['ellenfel3']['nev'],
+                         Kaland['kaland'][Player.lokacio]['ellenfel3']['HP'],
+                         Kaland['kaland'][Player.lokacio]['ellenfel3']['ugyesseg'])
     if Player.halott:
         break                                                                                                 
-    if advDict['kaland'][Player.lokacio]['akcio'] == "gyozelem":
+    if Kaland['kaland'][Player.lokacio]['akcio'] == "gyozelem":
         Nyert = True
         break
-    elif advDict['kaland'][Player.lokacio]['akcio'] == "vege":
+    elif Kaland['kaland'][Player.lokacio]['akcio'] == "vege":
         Nyert = False
         Player.vege()
         break                                                                                      
-    if "e" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "e" in Kaland['kaland'][Player.lokacio]['akcio']:
         while True:
             if dobas()>4:
                 LepettEMar = True
                 break
             else:
                 Player.jatekosSebzes(2)
-    if "kn" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "kn" in Kaland['kaland'][Player.lokacio]['akcio']:
         if dobasok() > Player.Skill:
-            Player.lokv(advDict['kaland'][Player.lokacio]['ugras'][0])
+            Player.lokv(Kaland['kaland'][Player.lokacio]['ugras'][0])
             LepettEMar = True
         else:
-            Player.lokv(advDict['kaland'][Player.lokacio]['ugras'][1])
+            Player.lokv(Kaland['kaland'][Player.lokacio]['ugras'][1])
             LepettEMar = True
-    if "proba" in advDict['kaland'][Player.lokacio]['akcio']:
+    if "proba" in Kaland['kaland'][Player.lokacio]['akcio']:
         if proba():
-            Player.lokv(advDict['kaland'][Player.lokacio]['HV'])
+            Player.lokv(Kaland['kaland'][Player.lokacio]['HV'])
             LepettEMar = True
     if not probaltemar:
-        if "gyurup" in advDict['kaland'][Player.lokacio]['akcio']:
+        if "gyurup" in Kaland['kaland'][Player.lokacio]['akcio']:
             print("Szeretnéd felprobálni a gyűrűt?")
             if igenvagynem():
-                Player.lokv(advDict['kaland'][Player.lokacio]['haszeretne'])
+                Player.lokv(Kaland['kaland'][Player.lokacio]['haszeretne'])
                 LepettEMar = True
                 probaltemar = True
     if not felkopeny:
-        if "kopeny" in advDict['kaland'][Player.lokacio]['akcio']:
+        if "kopeny" in Kaland['kaland'][Player.lokacio]['akcio']:
             print("Szeretnéd felprobálni a köpenyt?")
             if igenvagynem():
-                Player.lokv(advDict['kaland'][Player.lokacio]['haszeretnekopenyt'])
+                Player.lokv(Kaland['kaland'][Player.lokacio]['haszeretnekopenyt'])
                 LepettEMar = True
                 felkopeny = True
-    if "targy" in advDict['kaland'][Player.lokacio]['akcio']:
-        if (advDict['kaland'][Player.lokacio]['TI']['szukseges']) in Player.Items:
-            Player.lokv(advDict['kaland'][Player.lokacio]['TI']['HV'])
+    if "targy" in Kaland['kaland'][Player.lokacio]['akcio']:
+        if (Kaland['kaland'][Player.lokacio]['TI']['szukseges']) in Player.Items:
+            Player.lokv(Kaland['kaland'][Player.lokacio]['TI']['HV'])
             LepettEMar = True
     if not LepettEMar:
-        if len(advDict['kaland'][Player.lokacio]['ugras']) == 1:
+        if len(Kaland['kaland'][Player.lokacio]['ugras']) == 1:
             StartInp = input("folytatáshoz nyomj entert vagy írd be valamelyiket stat,kilep : ")
             if StartInp == "stat":
                 print(Player)
                 print("")
             elif StartInp == "kilep":
                 exit()
-            Player.lokv(advDict['kaland'][Player.lokacio]['ugras'][0])
-        elif len(advDict['kaland'][Player.lokacio]['ugras']) > 1:
+            Player.lokv(Kaland['kaland'][Player.lokacio]['ugras'][0])
+        elif len(Kaland['kaland'][Player.lokacio]['ugras']) > 1:
             print("Irja be merre szeretne haladni vagy irjon be egy commandot [stat, kilep]?")
             while True:
                 inp = input()
                 if not inp == "":
-                    if inp in str(advDict['kaland'][Player.lokacio]['ugras']):
+                    if inp in str(Kaland['kaland'][Player.lokacio]['ugras']):
                         Player.lokv(inp)
                         break
                     elif inp == "stat":
